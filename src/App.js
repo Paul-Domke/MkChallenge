bfb5b409b0a3486cf5114542b88eaca1e55706a3
Paul-Domke <paul.domke17@ncf.edu>
2021-09-03 09:04:11 -0700

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, TextField, Paper, Box, Grid} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  buttonStyle:{
+    color: 'primary'
+  },
+  root: {
+    '& .MuiTextField-root': {
+      width: '95%',
+      margin: theme.spacing(2),
+      justifyContent: 'center'
+    },
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React please!
-        </a>
-      </header>
-    </div>
+    <Box display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh" 
+      className={classes.root}>
+        
+      <Paper elevation="1">
+
+      <form autoComplete="off">
+
+        <Grid container>
+
+          <TextField
+            id="name-input"
+            label="Name"
+            value=''
+          />
+
+          <TextField
+            id="email-input"
+            label="Email"
+            type="email"
+          />
+
+          <TextField
+            id="message-input"
+            label="Message"
+            multiline
+            rows={4}
+            defaultValue=""
+            variant="outlined"
+          />
+
+          <Grid xs='5'/>
+
+          <Grid item justifyContent='center'>
+            <Button>
+              Send
+            </Button>
+          </Grid>
+
+          <Grid xs='5'/>
+
+        </Grid>
+        </form>
+      </Paper>
+    </Box>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Paper, Box } from '@material-ui/core';
+import { palette } from '@material-ui/system';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <Box bgcolor="text.secondary">
+        <App />
+        </Box>
   </React.StrictMode>,
   document.getElementById('root')
 );
